50920b831b94d0fc479eab8265a4871d7a36ec24
Zetxlix <93633598+Zetxlix@users.noreply.github.com>
2022-07-08 23:14:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -25,11 +25,11 @@ const fs = require("fs")
 const chalk = require("chalk")
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = true //auto reading in gc (true to on, false to off)
-global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoReadAll = true // auto reading in all pchat gc and status (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //documents variants
@@ -41,26 +41,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['19179009987'] //ur owner number
+global.ownername = "AᒪE᙭ᗩ"
+global.ytname = "YT: ALEXA YT" //ur yt chanel name
+global.socialm = "GitHub: Zetxlix" //ur github or insta name
+global.location = "Indonesia, Jawa Barat, Jakarta" //ur location
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
-global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
+global.botname = "AᒪE᙭ᗩ BOTᘔ༂" //ur bot name
+global.websitex = "https://www.youtube.com/channel/UCEHzs2xqkjpDpdpjfak_Wzw" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
+global.packname = "AᒪE᙭ᗩ BOTᘔ༂ Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
+global.author = "AᒪE᙭ᗩ BOTᘔ༂"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
-global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
-global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
-global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
+global.ownertag = ['19179009987'] //ur owner tag
+global.ownernummenu = ['19179009987'] //ur owner number in menu and all
+global.watermark = "AᒪE᙭ᗩ BOTᘔ༂." //ur watermark
+global.botscript = "https://wa.me/qr/D26JAA2RXJYDP1"
+global.linkz1 = "https://chat.whatsapp.com/E3m4zWG9IpO6Bz2AMs1sOM"
+global.linkz2 = "https://www.instagram.com/hyzzdhenta_"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
